7453f35eeb5cdd0cfe8b26ba7de67425187dcbed
Kian Merchant <supesu.dev@gmail.com>
2023-08-06 07:43:26 +1200

    remove callback

--- app/actions.tsx
+++ app/actions.tsx
@@ -1,30 +1,18 @@
-"use client";
-import { signIn, signOut } from "next-auth/react";
+"use client"
+import { signIn, signOut } from "next-auth/react"
 
 export function SignOut() {
   return (
-    <div
-      className={
-        "w-[10rem] py-auto mt-[2.5rem] cursor-pointer bg-slate-800 rounded-md h-[2.5rem] text-white font-bold uppercase flex"
-      }
-      onClick={() => signOut({ callbackUrl: `${process.env.CALLBACK_URL}` })}
-    >
+    <div className={"w-[10rem] py-auto mt-[2.5rem] cursor-pointer bg-slate-800 rounded-md h-[2.5rem] text-white font-bold uppercase flex"} onClick={() => signOut()}>
       <p className="mx-auto my-auto">Sign out</p>
     </div>
-  );
+  )
 }
 
 export function SignIn() {
   return (
-    <div
-      className={
-        "w-[10rem] py-auto mt-[2.5rem] cursor-pointer bg-slate-800 rounded-md h-[2.5rem] text-white font-bold uppercase flex"
-      }
-      onClick={() =>
-        signIn("github", { callbackUrl: `${process.env.CALLBACK_URL}` })
-      }
-    >
+    <div className={"w-[10rem] py-auto mt-[2.5rem] cursor-pointer bg-slate-800 rounded-md h-[2.5rem] text-white font-bold uppercase flex"} onClick={() => signIn("github")}>
       <p className="mx-auto my-auto">Sign in</p>
     </div>
-  );
+  )
 }
