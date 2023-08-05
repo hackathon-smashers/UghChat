670f2ad7e15b0b0123addb4604f71ce6dcb67295
Sebastian Thomas <stho382@aucklanduni.ac.nz>
2023-08-05 17:05:52 +1200

    added user file

--- frontend/pages/api/user.ts
+++ frontend/pages/api/user.ts
@@ -1,17 +1,24 @@
 import { getToken, JWT } from "next-auth/jwt"
+import { getApp } from "firebase/app";
+import { getDatabase, onValue, ref } from "firebase/database";
 
 const secret = process.env.NEXTAUTH_SECRET
+const app = getApp()
+const db = getDatabase(app)
 
 export default async function handler(req: any, res: any) {
-    // const token = await getToken({req, secret})
 
-    // if (!token) {
-    //     return res.status(401).json({message: "Unauthenticated."})
-    // }
+    console.log(db)
 
-    
+    const someRef = ref(db, 'users/');
+    onValue(someRef, (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+        return res.status(200).json({message: JSON.stringify(data)})
+    });
 
-    return res.status(200).json({message: "hello"})
+
+    return res.status(200).json({message: "iasjdoiasjdoiasjd"})
 
 
 
