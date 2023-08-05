bba3603c0ea8cff6b70d96577655530b109a0c63
Kian Merchant <supesu@wifi-172-23-35-41.uoa-wifi.auckland.ac.nz>
2023-08-05 20:42:12 +1200

    doing some tings

--- frontend/app/actions.tsx
+++ frontend/app/actions.tsx
@@ -12,7 +12,7 @@ export function SignOut() {
 
 export function SignIn() {
   return (
-    <div className={"w-[10rem] py-auto cursor-pointer bg-slate-800 rounded-md h-[2.5rem] text-white font-bold uppercase flex"} onClick={() => signIn("github")}>
+    <div className={"w-[10rem] py-auto mt-[2.5rem] cursor-pointer bg-slate-800 rounded-md h-[2.5rem] text-white font-bold uppercase flex"} onClick={() => signIn("github")}>
       <p className="mx-auto my-auto">Sign in</p>
     </div>
   )
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,6 +4,8 @@ import { useSession } from "next-auth/react";
 import { SignIn, SignOut } from "./actions";
 import { useEffect } from "react"
 import { useDatabase } from "../hooks/useDatabase"
+import Image from "next/image"
+import { redirect } from "next/navigation";
 
 export default function IndexPage() {
   const { status } = useSession()
@@ -17,13 +19,71 @@ export default function IndexPage() {
     setUser(user + "a")
   }, [status])
 
+  if (status == "authenticated") redirect("/hub")
+
   return (
-    <div className="w-full min-h-screen bg-[#f2f2f3] h-screen">
-      <p className="flex text-4xl text-slate-900 font-bold mx-auto justify-center pt-[10rem]">UghChat</p>
-      <img src="https://cdn-icons-png.flaticon.com/512/773/773328.png" alt="" className="mx-auto mt-4 w-[10rem]" />
-      <div className="flex justify-center mt-[2rem]">
-        {status === "authenticated" ? <SignOut /> : <SignIn />}
+    <div className="flex flex-col w-full h-screen min-h-screen">
+      <div className="flex w-full h-[5rem] pt-4">
+        {/* Header */}
+        <div className="flex ml-[1.2rem] h-[2rem] w-[2rem] bg-slate-400 rounded-md rounded-tl-none"></div>
+        <p className="flex ml-[.6rem] text-2xl font-bold text-slate-900">UghChat</p>
+
+
+        <ul className="ml-auto flex flex-row mr-[1.2rem]">
+          <li className="mr-[2rem] ">
+            <a target="_blank" className="flex cursor-pointer flex-row font-medium text-[#65686C] hover:font-bold hover:text-black" href="https://github.com/00dsql">Don</a>
+          </li>
+          <li className="mr-[2rem] w-[4.43rem] flex">
+            <a className="flex cursor-pointer flex-row font-medium text-[#65686C] hover:font-bold hover:text-black" href="https://github.com/derogative404">Sebastian</a>
+          </li>
+          <li className="mr-[2rem] w-[2.7rem]">
+            <a className="flex cursor-pointer flex-row font-medium text-[#65686C] hover:font-bold hover:text-black" href="https://github.com/LetMeP1ay">Vadim</a>
+          </li>
+          <li className="mr-[2rem] w-[2rem]">
+            <a className="flex cursor-pointer flex-row font-medium text-[#65686C] hover:font-bold hover:text-black" href="https://github.com/supesu">Kian</a>
+          </li>
+          <li className="mr-[1.1rem] w-[1.6rem]">
+            <a target="_blank" className="flex cursor-pointer flex-row font-medium text-[#65686C] hover:font-bold hover:text-black" href="#">Jin</a>
+          </li>
+        </ul>
+
+      </div>
+      <div className="flex h-full w-full">
+        <div className="flex flex-col w-1/2 h-full">
+          {/* Lefthand side */}
+
+          <div className="flex flex-col w-[40rem] m-auto ml-[5rem] mt-[15rem]">
+            <p className="text-5xl font-bold">Say it with a grunt!</p>
+            <p className="text-xl font-light w-[25rem] text-slate-500 mt-3">Text to Grunt: Because sometimes words are just too mainstream.</p>
+            <SignIn />
+          </div>
+        </div>
+        <div className="flex w-1/2 h-full">
+          {/* Righthand side */}
+
+          <Image
+            className="mx-auto mt-[1.2rem] aspect-[1.07/1] hide-bg bg-no-repeat p-8 pb-12 w-[41rem] h-[40rem]"
+            height={400}
+            width={400}
+            priority
+            style={{ backgroundImage: 'url(/blob.svg)' }}
+            src="/HeroImage.png"
+            alt="Hero image"
+          />
+
+        </div>
       </div>
     </div>
   )
+
+
+  // return (
+  //   <div className="w-full min-h-screen bg-[#f2f2f3] h-screen">
+  //     <p className="flex text-4xl text-slate-900 font-bold mx-auto justify-center pt-[10rem]">UghChat</p>
+  //     <img src="/hero.jpg" alt="" className="hide-bg mx-auto mt-4 w-[14rem] h-[14rem]" />
+  //     <div className="flex justify-center mt-[1.5rem]">
+  //       <SignIn/>
+  //     </div>
+  //   </div>
+  // )
 }
