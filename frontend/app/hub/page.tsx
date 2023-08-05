d28e3598435abe06c7fc89224c32dd29f5edffb0
Vadim <vdaberezin@gmail.com>
2023-08-05 17:13:28 +1200

    frontend messaging page update

--- frontend/app/hub/page.tsx
+++ frontend/app/hub/page.tsx
@@ -1,31 +1,52 @@
 "use client"
 
-import { getSession } from "next-auth/react";
+import { useSession } from "next-auth/react";
 import { useEffect } from "react";
+import RightChevron from "../../ui/icons/RightChevron";
 
-interface User {
-    name: string;
-    pfp: string;
-    status: boolean;
+const User = ({ data, status }: any) => {
+    return (
+        <div className="w-96 bg-white h-[6.4rem] border-2 border-slate-200 rounded-md flex flex-row">
+            <div className="flex flex-row m-3">
+
+                <ul>
+                    <div
+                        className="w-16 h-16 bg-cover bg-center rounded-full"
+                        style={{
+                            backgroundImage: `url('${data.user?.image}')`,
+                        }}>
+                    </div>
+                </ul>
+
+                <ul className="m-3 ml-[1.5rem] my-[0rem] my-auto overflow-x-scroll">
+                    <li className="text-md font-bold">{data.user!.name}</li>
+                    <li className="italic">of the dank meme tribe</li>
+                    <li>
+                        <span
+                            className={`inline-block w-2 h-2 rounded-full ${status === "authenticated" ? "bg-green-400" : "bg-gray-300"
+                                } animate-pulse mr-1`}
+                        ></span>
+                        {status === "authenticated" ? "Online" : "Offline"}
+                    </li>
+                </ul>
+                <div>
+                    <RightChevron />
+                </div>
+            </div>
+        </div>
+
+    )
 }
 
 export default function IndexPage() {
-    const session = getSession();    
-
-    useEffect(() => {
-        console.log(session)
-    }, [session])
+    const { data, status } = useSession();
+    if (status !== "authenticated") return <></>
 
     return (
-        <div className="w-full min-h-screen bg-black h-screen flex justify-center items-center">
-            <div className="w-96 bg-white h-screen">
-                <ul>
-                    <div className="justify-center">
-                    <li>hello</li>
-                    <li>yo</li>
-                    </div>
-                </ul>
-            </div>
+        <div className="min-h-screen bg-white flex flex-col space-y-[0.8rem] justify-center items-center">
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(() => (
+                <User data={data} status={status} />
+            ))}
         </div>
-    )
+    );
 }
---
+++ frontend/ui/icons/RightChevron.tsx
@@ -0,0 +1,7 @@
+export default function RightChevron() {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="w-[1em] h-[1em] inline-block fill-current" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"></path>
+        </svg>
+    )
+}
