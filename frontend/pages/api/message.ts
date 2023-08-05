b6128a628598362f3c929c572425dda028a98e87
Sebastian Thomas <stho382@aucklanduni.ac.nz>
2023-08-05 18:18:45 +1200

    added message interface

--- frontend/pages/api/message.ts
+++ frontend/pages/api/message.ts
@@ -4,3 +4,32 @@ import { getDatabase, onValue, ref } from "firebase/database";
 const secret = process.env.NEXTAUTH_SECRET
 const app = getApp()
 const db = getDatabase(app)
+
+interface ChatMessage {
+    chatID:{
+        messageID: {
+            message: string;
+            messageDate: string;
+            messageTime: string;
+            sentBy: string;
+        };
+    }
+}
+
+interface SendMessageFormProps {
+    chatId: string;
+    userId: string;
+}
+
+export default async function handler(req: any, res: any) {
+
+    const someRef = ref(db, 'message/');
+    onValue(someRef, (snapshot) => {
+        const data = snapshot.val();
+        console.log(data);
+        return res.status(200).json({message: JSON.stringify(data)})
+    });
+
+
+    return someRef;
+}
