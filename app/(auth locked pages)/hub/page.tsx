e92bb08046e66385ddb08b22d8581e149667472d
Vadim <vdaberezin@gmail.com>
2023-08-06 11:36:07 +1200

    something

--- app/(auth locked pages)/hub/page.tsx
+++ app/(auth locked pages)/hub/page.tsx
@@ -4,7 +4,7 @@ import { HubClient } from "../../../components/hub.client";
 
 export default function Page() {
   return (
-    <div className="min-h-full min-w-full bg-white flex flex-col space-y-2 lg:space-y-[0.8rem] lg:justify-center lg:items-center overflow-visible">
+    <div className="h-auto w-auto bg-white flex flex-col space-y-2 lg:space-y-[0.8rem] lg:justify-center lg:items-center overflow-visible">
       <div className="flex ml-auto mr-auto flex-col">
         <div className="flex flex-row">
           <h1 className="font-bold text-2xl mr-auto lg:text-3xl lg:mt-[2.5rem] text-slate-900">
