75927fdc03e78a54d9ff2e1e01e8cefd6d82d7cc
Kian Merchant <supesu.dev@gmail.com>
2023-08-06 07:33:55 +1200

    change the provider account id name

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,6 @@
-import { FirestoreAdapter } from '@next-auth/firebase-adapter';
-import NextAuth from 'next-auth';
-import GithubProvider from 'next-auth/providers/github';
+import { FirestoreAdapter } from "@next-auth/firebase-adapter";
+import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -17,16 +17,16 @@ export const authOptions = {
     projectId: process.env.FIREBASE_PROJECT_ID,
     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        databaseURL: process.env.FIREBASE_DATABASE_URL
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
   }),
   callbacks: {
     async session({ session, user, token }) {
-      if (session.user) session.user.discordID = token.discordId;
+      if (session.user) session.user.userId = token.userId;
 
-      return { discordId: token.discordId, ...session };
+      return { discordId: token.userId, ...session };
     },
     async jwt({ token, user, account, profile, isNewUser }) {
-      if (account) token.discordId = account.providerAccountId;
+      if (account) token.userId = account.providerAccountId;
 
       return Promise.resolve(token);
     },
@@ -38,19 +38,19 @@ export const authOptions = {
         iat: Date.now() / 1000,
         exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60,
       };
-      const encodedToken = jwt.sign(jwtClaims, secret, { algorithm: 'HS256' });
+      const encodedToken = jwt.sign(jwtClaims, secret, { algorithm: "HS256" });
       return encodedToken;
     },
     decode: async ({ secret, token, maxAge }) => {
-      const decodedToken = jwt.verify(token, secret, { algorithms: ['HS256'] });
+      const decodedToken = jwt.verify(token, secret, { algorithms: ["HS256"] });
       return decodedToken;
     },
   },
   session: {
-    strategy: 'jwt',
+    strategy: "jwt",
   },
   pages: {
-    signIn: '/',
+    signIn: "/",
   },
 };
 export default NextAuth(authOptions);
