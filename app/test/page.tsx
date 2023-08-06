960200929db47ebbd4378266e5daaa8db72e4782
Sebastian Thomas <stho382@aucklanduni.ac.nz>
2023-08-06 13:44:20 +1200

    added stuff

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+import '../../components/smokeeffect/SmokeEffect'
+import SmokeEffect from '../../components/smokeeffect/SmokeEffect'
+
+export default function Page() {
+    return (
+        <div className="flex w-full h-screen min-h-screen bg-gray-30">
+            <SmokeEffect />
+        </div>
+    )
+}
