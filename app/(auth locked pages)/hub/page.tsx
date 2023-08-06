15aaf06023945cd20ac54264d9ea91ccc155773a
Sebastian Thomas <stho382@aucklanduni.ac.nz>
2023-08-06 15:07:49 +1200

    added to pages element

--- app/(auth locked pages)/hub/page.tsx
+++ app/(auth locked pages)/hub/page.tsx
@@ -2,9 +2,12 @@ import { SignOut } from "../../actions";
 
 import { HubClient } from "../../../components/hub.client";
 
+import Smoke from "../../../components/smokeeffect/SmokeEffect";
+
 export default function Page() {
   return (
-    <div className="h-screen w-full mx-[4rem] bg-white flex flex-col space-y-2 lg:space-y-[0.8rem] justify-center items-center">
+    <div>
+      <div className="h-screen w-full mx-[4rem] bg-white flex flex-col space-y-2 lg:space-y-[0.8rem] justify-center items-center">
       <div className="flex mx-[4rem] flex-col flex-grow">
         <div className="flex flex-row my-[1rem]">
           <h1 className="font-bold text-2xl mr-auto lg:text-3xl lg:mt-[2.5rem] text-slate-900">
@@ -15,6 +18,9 @@ export default function Page() {
 
         <HubClient />
       </div>
+    </div>
+    <Smoke/>
+
     </div>
   );
 }
