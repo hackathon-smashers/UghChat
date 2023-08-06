f6dfe883e5f6c32c9d82d17dcfb7a60b62946f44
Sebastian Thomas <stho382@aucklanduni.ac.nz>
2023-08-06 14:28:54 +1200

    added smoke effect

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,10 +1,22 @@
+"use client"
+
 import '../../components/smokeeffect/SmokeEffect'
 import SmokeEffect from '../../components/smokeeffect/SmokeEffect'
+// import SmokeElement from "smoke-effect-react";
+
+// className="flex w-full h-screen min-h-screen bg-gray-30"
+
 
 export default function Page() {
     return (
-        <div className="flex w-full h-screen min-h-screen bg-gray-30">
-            <SmokeEffect />
+        <div style={{ width: "100vw", height: "100vh", display: "flex", alignItems: "center", justifyContent: "center" }}>
+             {/* <SmokeElement
+                src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/quickText.png"
+                opacity="1"
+                smokeSrc="https://s3-us-west-2.amazonaws.com/s.cdpn.io/95637/Smoke-Element.png"
+                smokeOpacity="0.3"
+                /> */}
+                <SmokeEffect />
         </div>
     )
 }
